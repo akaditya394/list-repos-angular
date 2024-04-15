ff697b0e5deec9fbfdb9676acffa8a3ea307bbe2
Aditya <akaditya394@gmail.com>
2024-04-15 17:04:16 +0530

    css error fixed

--- src/app/components/repo-details/repo-details.component.ts
+++ src/app/components/repo-details/repo-details.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-repo-details',
   templateUrl: './repo-details.component.html',
-  styleUrls: ['./repo-details.component.css'],
+  styleUrls: ['./repo-details.component.scss'],
 })
 export class RepoDetailsComponent {
   @Input() repo: any;
