91358ab092af62b79cf910fd6426f33e2b60365d
Aditya <akaditya394@gmail.com>
2024-04-15 16:34:43 +0530

    main app functionalities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit{
-  constructor(
-    private apiService: ApiService
-  ) {}
+export class AppComponent {
+  username: string = '';
+  repositories: any[] = [];
+  loading: boolean = false;
+  pageSize: number = 10;
+  currentPage: number = 1;
 
-  ngOnInit() {
-    this.apiService.getUser('johnpapa').subscribe(console.log);
+  constructor(private apiService: ApiService) {}
+
+  search() {
+    this.loading = true;
+    this.currentPage = 1; // Reset current page when searching
+    this.loadRepositories();
+  }
+
+  loadRepositories() {
+    this.apiService
+      .getRepositories(this.username, this.currentPage, this.pageSize)
+      .subscribe((data: any[]) => {
+        this.repositories = data;
+        this.loading = false;
+      });
+  }
+
+  nextPage() {
+    this.currentPage++;
+    this.loadRepositories();
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.loadRepositories();
+    }
+  }
+
+  onPageSizeChange(event: any) {
+    const pageSize = event.target.value;
+    this.pageSize = parseInt(pageSize, 10); // Parse the value as an integer
+    this.loadRepositories();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from  '@angular/common/http';
+import { ApiService } from './services/api.service';
+import { RepoDetailsComponent } from './components/repo-details/repo-details.component'; // Import ApiService here
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, RepoDetailsComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  providers: [ApiService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
